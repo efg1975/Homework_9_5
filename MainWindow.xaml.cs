d16314b5ae9ca068d839fc354d758ec57d668000
efg1975 <efg1975sm@gmail.com>
2023-03-13 12:01:34 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework_9_5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        static string ReverseWords(string text)
+        {
+            string[] words = SplitText(text);
+            string sortText = SortWords(words);
+            return sortText;
+        }
+
+        static string SortWords(string[] text)
+        {
+            Array.Reverse(text);
+            string text1 = string.Join(" ", text);
+            return text1;
+        }
+        public static string[] SplitText(string text)
+        {
+            string[] words = text.Split(' ');
+            return words;
+        }
+
+        private void Words_Click(object sender, RoutedEventArgs e)
+        {
+            ListWords.Items.Clear();
+            foreach (var item in SplitText(UserStringWords.Text))
+            {
+                ListWords.Items.Add(item);
+            }
+        }
+
+        private void Reverse_Click(object sender, RoutedEventArgs e)
+        {
+            ReverseString.Content = ReverseWords(UserStringString.Text);
+        }
+    }
+}
+
+    
+
